5922afe234a50bc2a72ca793f77ed9e7f774bf58
Mohammed Elobadi <elobadi@gmail.com>
2018-09-17 22:09:39 +0200

    034 Solution - Add a social media menu to the footer

--- humescores/functions.php
+++ humescores/functions.php
@@ -44,7 +44,8 @@ if ( ! function_exists( 'humescores_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'header', 'humescores' ),
+			'menu-1' => esc_html__( 'Header', 'humescores' ),
+			'social-menu' => esc_html__( 'Social Media Menu', 'humescores' ),
 		) );
 
 		/*
